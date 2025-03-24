84dd6e2bd547f14f2e26eb82fa2c44e4dd8bdf63
Liam Ilan <snowboardsheep@gmail.com>
2025-03-24 03:42:49 -0700

    Implement flow rate sensor on RSM.

--- src/formula_e_hil/rsm_fakes.py
+++ src/formula_e_hil/rsm_fakes.py
@@ -1,3 +1,5 @@
+import threading
+import time
 from .ssm import Ssm
 
 
@@ -25,6 +27,40 @@ class RsmFakes:
         # Tie the pump ground channel to ground.
         self._ssm_handler.set_analog(self._PUMP_GROUND_CHANNEL, 0)
 
+        # Start flow rate pwm at 0 Hz.
+        self._flow_rate_pwm_freq_hz = 0
+
+        # Setup PWM background loop.
+        def pwm_loop():
+
+            # Run a background PWM loop.
+            flow_rate_last_cycle_secs = time.time()
+            while not self._pwm_exit_event.is_set():
+
+                # Don't update flow rate if requested frequency is 0.
+                if self._flow_rate_pwm_freq_hz != 0:
+
+                    # Calculate requested period.
+                    flow_rate_period_secs = 1 / self._flow_rate_pwm_freq_hz
+
+                    # Set 50% duty cycle on flow rate PWM.
+                    time_since_last_cycle_secs = time.time() - flow_rate_last_cycle_secs
+                    if time_since_last_cycle_secs <= flow_rate_period_secs / 2:
+                        self._ssm_handler.set_data_out(self._FLOW_RATE_PWM, True)
+                    elif flow_rate_period_secs / 2 < time_since_last_cycle_secs <= flow_rate_period_secs:
+                        self._ssm_handler.set_data_out(self._FLOW_RATE_PWM, False)
+                    else:
+                        flow_rate_last_cycle_secs = time.time()
+        
+        # Spawn the pwm loop in a background thread.
+        self._pwm_exit_event = threading.Event()
+        self._pwm_thread = threading.Thread(target = pwm_loop)
+
+    def __exit__(self):
+        # Make sure PWM thread closes when the class destructs.
+        self._pwm_exit_event.set()
+        self._pwm_thread.join()
+
     def set_suspension_travel_left(self, value: float):
         self._ssm_handler.set_analog(self._SUSPENSION_TRAVEL_LEFT_CHANNEL, value)
 
@@ -33,3 +69,13 @@ class RsmFakes:
 
     def set_rear_brake_pressure(self, value: float):
         self._ssm_handler.set_analog(self._REAR_BRAKE_PRESSURE_CHANNEL, value)
+
+    def set_flow_rate(self, rate_litres_per_min: float):
+        """Set flow rate sensor.
+
+        Args:
+            rate_litres_per_min: Litres per minute to report as the flow rate.
+        
+        """
+
+        self._flow_rate_pwm_freq_hz = 7.5 * rate_litres_per_min
