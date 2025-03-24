295efad7dff6104d292ff62c6331e7230f538161
Liam Ilan <snowboardsheep@gmail.com>
2025-03-23 20:31:05 -0700

    Clean up SSM abstraction.

--- src/formula_e_hil/ssm.py
+++ src/formula_e_hil/ssm.py
@@ -3,12 +3,18 @@ from enum import Enum
 
 
 class SSM:
+    # ISOSPI high side and low side Chimera IDs.
     _ISOSPI_HIGH_SIDE_NAME = "SPI_ISOSPI_HS"
     _ISOSPI_LOW_SIDE_NAME = "SPI_ISOSPI_LS"
 
+    # Maximum voltage out of the SSM DAC.
     DAC_REF_VOLTS = 5
+
+    # DAC spi and clear (active low) Chimera IDs.
     _DAC_NAME = "SPI_DAC"
     _DAC_N_CLEAR_NAME = "GPIO_DAC_N_CLEAR"
+
+    # Resolution of the LTC2620, 12 bits.
     _DAC_RESOLUTION = 12
 
     def __init__(self):
@@ -106,19 +112,19 @@ class SSM:
 
         self._chimera_handler.gpio_write(self._BOOT_LED_NAME, status)
 
-    class DacChannel(Enum):
-        """Representation of a DAC channel."""
+    class AnalogOut(Enum):
+        """Representation of an output analog channel. Maps from channel to DAC channel id."""
 
-        A = 0b00
-        B = 0x01
-        C = 0x02
-        D = 0x03
-        E = 0x04
-        F = 0x05
-        G = 0x06
-        H = 0x07
+        ONE = 0b00  # VOUT A.
+        TWO = 0x01  # VOUT B.
+        THREE = 0x02  # VOUT C.
+        FOUR = 0x03  # VOUT D.
+        FIVE = 0x07  # VOUT H.
+        SIX = 0x06  # VOUT G.
+        SEVEN = 0x05  # VOUT F.
+        EIGHT = 0x04  # VOUT E.
 
-    def dac_transmit(self, channel: DacChannel, out_volts: float):
+    def dac_transmit(self, channel: AnalogOut, out_volts: float):
         """Transmit a voltage over a channel of the onboard LTC2620.
 
         Args:
@@ -126,24 +132,34 @@ class SSM:
             out_volts: Voltage to output, capped at the reference voltage (SSM.DAC_REF_VOLTS).
 
         """
-        assert out_volts <= self.DAC_REF_VOLTS
 
         # DAC driver for LTC2620, from the datasheet:
         # https://datasheet.ciiva.com/pdfs/VipMasterIC/IC/LITC/LITCS09782/LITCS09782-1.pdf?src-supplier=IHS+Markit
 
         # 0b0011 writes to input register, and updates DAC register in one command.
         command_bits = 0b0011
+
+        # Extract channel from AnalogOut enum.
         channel_bits = channel.value
 
         # From V_OUT = (k / 2^N) V_REF in the datasheet.
-        # where k is the data, N is the resolution, and V_REF is the reference voltage.
+        # ie. k = (V_OUT / V_REF) * 2^N.
+        # where k is the setpoint, N is the resolution, and V_REF is the reference voltage.
         data_bits = int((out_volts / self.DAC_REF_VOLTS) * (2**self._DAC_RESOLUTION))
+
+        # Make sure data_bits / requested output are legal.
+        assert out_volts <= self.DAC_REF_VOLTS
+        assert data_bits <= (2**self._DAC_RESOLUTION)
 
         # Build word and convert to bytes.
         # Input word format:
-        # [ command (4) | channel (4) | data     (12) | UNUSED   (4) ]
-        word = command_bits + channel_bits << 4 + data_bits << 8
-        word_bytes = word.to_bytes(4, "big")
+        # 24-----------20------------16-------------4-------------0
+        # [ command (4) | channel (4) | data   (12) | UNUSED  (4) ]
+        # 3 bytes long, most-significant bit to the left.
+        input_word = (
+            command_bits << 20 + channel_bits << 16 + data_bits << 4 + 0b0000 << 0
+        )
+        input_word_bytes = input_word.to_bytes(3, "big")
 
         # Transmit.
-        self._dac_handler.transmit(word_bytes)
+        self._dac_handler.transmit(input_word_bytes)
