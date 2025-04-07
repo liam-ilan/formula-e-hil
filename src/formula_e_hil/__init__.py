e99bf3c06d2fd40bb2880f537b87c5c97a86d514
Liam Ilan <snowboardsheep@gmail.com>
2025-04-07 01:54:00 -0700

    Setup HIL wrapper.

--- src/formula_e_hil/__init__.py
+++ src/formula_e_hil/__init__.py
@@ -1,3 +1,18 @@
-from .fsm_fakes import FsmFakes as FsmFakes
-from .rsm_fakes import RsmFakes as RsmFakes
-from .can import Can as Can
+from .fsm_fakes import FsmFakes
+from .rsm_fakes import RsmFakes
+from .can import Can
+import can
+
+
+class Hil:
+    def __init__(self, can_bus_handle: can.BusABC):
+        """Wrapper for the HIL system.
+
+        Args:
+            can_bus_handle: python-can CAN bus handle.
+
+        """
+
+        self.fsm_fakes = FsmFakes()
+        self.rsm_fakes = RsmFakes()
+        self.can_bus = Can(can_bus_handle)
